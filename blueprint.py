df1169db8714083f2320e65704afdf3956c3d6f5
AnastasiiaKudybovska <113639401+AnastasiiaKudybovska@users.noreply.github.com>
2022-10-03 05:56:47 +0000

    Delete blueprint.py

--- blueprint.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-api_blueprint = Blueprint('api',__name__)
-STUDENT_ID=3
-@api_blueprint.route("/hello-world")
-def hello_world_def():
-    return f"Hello world!!!"
-@api_blueprint.route(f"/hello-world-9")
-def hello_world():
-    return f"Hello, World 9"
